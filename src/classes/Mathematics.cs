58f76e904818bb527943eb3d91299f55f056872e
bouajilaprog <bouajilamedyessine@gmail.com>
2026-04-20 12:12:45 +0100

    chore: move source code to a sub-folder

---
+++ src/classes/Mathematics.cs
@@ -0,0 +1,23 @@
+﻿namespace Bouajila.Incidents.Api;
+
+public class Mathematics
+{
+    public int Factorial(int n)
+    {
+        if (n < 0)
+        {
+            throw new ArgumentException("Input must be a non-negative integer.", nameof(n));
+        }
+        else
+        if (n == 0 || n == 1)
+        {
+            return n;
+        }
+        int result = 1;
+        for (int i = 2; i < n; i++)
+        {
+            result *= i;
+        }
+        return result;
+    }
+}
